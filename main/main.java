fe0a65ae61cacfa36a4cb09f919efa9ed6a1b42a
BaeFell <nfell2009@live.co.uk>
2016-08-07 20:55:25 +0100

    Create main.java

---
+++ main/main.java
@@ -0,0 +1,9 @@
+package main;
+
+public class main extends UmbaskaModule {
+
+  public void onEnable() {
+    Umbaska.getBukkit().broadcastMessage("This is just a very basic example (and for testing...). Do not take this repo seriously.");
+  }
+  
+}
